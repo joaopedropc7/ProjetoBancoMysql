66e1ca6ae0adbd8800523367a45642a75f23df16
joaopedropc7 <joaopedropcunha7@gmail.com>
2023-05-25 22:08:45 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -0,0 +1,111 @@
+package br.com.alura.bytebank.domain.conta;
+
+import br.com.alura.bytebank.ConnectionFactory;
+import br.com.alura.bytebank.domain.RegraDeNegocioException;
+import br.com.alura.bytebank.domain.cliente.Cliente;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContaService {
+
+    private ConnectionFactory connection;
+
+    public ContaService() {
+        this.connection = new ConnectionFactory();
+    }
+
+    public Set<Conta> listarContasAbertas() {
+        Connection conn = connection.recuperarConexao();
+        return new ContaDAO(conn).listar();
+    }
+
+    public BigDecimal consultarSaldo(Integer numeroDaConta) {
+        var conta = buscarContaPorNumero(numeroDaConta);
+        return conta.getSaldo();
+    }
+
+    public void abrir(DadosAberturaConta dadosDaConta) {
+        Connection conn = connection.recuperarConexao();
+        new ContaDAO(conn).salvar(dadosDaConta);
+
+    }
+
+    public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (valor.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RegraDeNegocioException("Valor do saque deve ser superior a zero!");
+        }
+
+        if (valor.compareTo(conta.getSaldo()) > 0) {
+            throw new RegraDeNegocioException("Saldo insuficiente!");
+        }
+
+        if(!conta.getEstaAtiva()) {
+            throw new RegraDeNegocioException("Conta não esta ativa");
+        }
+
+        Connection conn = connection.recuperarConexao();
+        ContaDAO contaDAO = new ContaDAO(conn);
+        BigDecimal novoValor = conta.getSaldo().subtract(valor);
+        contaDAO.alterar(conta.getNumero(), novoValor);
+
+    }
+
+    public void realizarDeposito(Integer numeroDaConta, BigDecimal valor) {
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (valor.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RegraDeNegocioException("Valor do deposito deve ser superior a zero!");
+        }
+        if(!conta.getEstaAtiva()) {
+            throw new RegraDeNegocioException("Conta não esta ativa");
+        }
+
+        Connection conn = connection.recuperarConexao();
+        ContaDAO contaDao = new ContaDAO(conn);
+        BigDecimal novoValor = conta.getSaldo().add(valor);
+        contaDao.alterar(conta.getNumero(), novoValor);
+    }
+
+    public void realizarTansferencia(Integer numeroDaConta, Integer numeroDaContaDestino, BigDecimal valor) {
+
+        this.realizarSaque(numeroDaConta, valor);
+        this.realizarDeposito(numeroDaContaDestino, valor);
+    }
+
+    public void encerrar(Integer numeroDaConta) {
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (conta.possuiSaldo()) {
+            throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
+        }
+
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).deletar(numeroDaConta);
+    }
+
+    public void encerrarLogico(Integer numeroDaConta){
+        var conta = buscarContaPorNumero(numeroDaConta);
+        if (conta.possuiSaldo()) {
+            throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
+        }
+
+        Connection conn = connection.recuperarConexao();
+        new ContaDAO(conn).alterarLogico(numeroDaConta);
+
+    }
+
+    private Conta buscarContaPorNumero(Integer numero) {
+        Connection conn = connection.recuperarConexao();
+        Conta conta = new ContaDAO(conn).listarPorNumero(numero);
+        if(conta != null) {
+            return conta;
+        } else {
+            throw new RegraDeNegocioException("Não existe conta cadastrada com esse número!");
+        }
+    }
+}
